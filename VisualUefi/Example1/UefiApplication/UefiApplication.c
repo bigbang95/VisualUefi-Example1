a2f3c1a8055ad74820ac032ef63a9c2572ab961b
bigbang95 <mail@qq.com>
2022-10-05 15:50:07 +0800

    VisualUefi 最简单例子 成功完成（包括 UEFI 应用程序 和 UEFI 运行时驱动）

---
+++ VisualUefi/Example1/UefiApplication/UefiApplication.c
@@ -0,0 +1,25 @@
+﻿#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+// 我们可以在任何 UEFI 规范上 运行它
+/*extern*/ CONST UINT32 _gUefiDriverRevision = 0;
+
+// 必须有
+// our name
+CHAR8* gEfiCallerBaseName = "Uefi App";
+
+// 必须有
+EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
+{
+	Print(L"Hello World!!!\n"); // 不支持中文字符串
+
+	return EFI_SUCCESS;
+}
+
+// 必须有
+EFI_STATUS EFIAPI UefiUnload(IN EFI_HANDLE ImageHandle)
+{
+	// your code
+
+	return EFI_SUCCESS;
+}
---
+++ VisualUefi/Example1/UefiDriver/UefiDriver.c
@@ -0,0 +1,25 @@
+﻿#include <Uefi.h>
+#include <Library/UefiLib.h>
+
+// 我们要求至少是 UEFI 2.0
+CONST UINT32 _gUefiDriverRevision = 0x200;
+
+// 必须有
+// our name
+CHAR8* gEfiCallerBaseName = "Uefi Driver";
+
+// 必须有
+EFI_STATUS EFIAPI UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
+{
+	Print(L"Hello World!!!\nDriver\n"); // 不支持中文字符串
+
+	return EFI_SUCCESS;
+}
+
+// 必须有
+EFI_STATUS EFIAPI UefiUnload(IN EFI_HANDLE ImageHandle)
+{
+	// your code
+
+	return EFI_SUCCESS;
+}
